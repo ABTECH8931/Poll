f503920a4572c035c96502d26e2ccf9a8be109cf
ABTECH8931 <abrahamefetobore4@gmail.com>
2025-04-25 13:58:47 -0700

    Update settings.py

--- Poll/settings.py
+++ Poll/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*189s%l-t82w2xovu3!ij079a81_tdg&u!_$j$3=rh1eql68(l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
